4bce0d24ae1ba0102ec2700b7775597f6121295f
Lucioschenkel <lucio.schenkel@proton.me>
2025-03-06 06:44:10 -0300

    feat: add split_node_delimiter

--- src/test_utils.py
+++ src/test_utils.py
@@ -1,9 +1,9 @@
 import unittest
 
-from utils import text_node_to_html_node
+from utils import text_node_to_html_node, split_nodes_delimiter
 from textnode import TextType, TextNode
 
-class TestTextNode(unittest.TestCase):
+class TestUtilsTextNodeToHtml(unittest.TestCase):
     def test_text(self):
         node = TextNode("This is a text node", TextType.TEXT)
         html_node = text_node_to_html_node(node)
@@ -15,6 +15,29 @@ class TestTextNode(unittest.TestCase):
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, "a")
         self.assertEqual(html_node.value, "This is a link node")
+
+class TestUtilsSplitNodesDelimiter(unittest.TestCase):
+    def test_single_text(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertListEqual(new_nodes, [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" word", TextType.TEXT),
+        ])
+
+    def test_multiple_nodes(self):
+        old_nodes = [
+            TextNode("node 1 `code`", TextType.TEXT),
+            TextNode("`code` node 2", TextType.TEXT),
+        ]
+        new_nodes = split_nodes_delimiter(old_nodes, "`", TextType.CODE)
+        self.assertListEqual(new_nodes, [
+            TextNode("node 1 ", TextType.TEXT),
+            TextNode("code", TextType.CODE),
+            TextNode("code", TextType.CODE),
+            TextNode(" node 2", TextType.TEXT),
+        ])
 
 if __name__ == "__main__":
     unittest.main()
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,4 @@
-from textnode import TextType
+from textnode import TextType, TextNode
 from htmlnode import LeafNode
 
 def text_node_to_html_node(text_node):
@@ -22,3 +22,17 @@ def text_node_to_html_node(text_node):
             })
 
     raise ValueError('Invalid text_node type')
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        split_nodes = node.text.split(delimiter)
+        nodes_to_add = filter(
+            lambda n: len(n.text) > 0,
+            list(
+                map(lambda e: TextNode(e[1], text_type) if e[0] % 2 != 0 else TextNode(e[1], TextType.TEXT), enumerate(split_nodes))
+            )
+        )
+        new_nodes.extend(nodes_to_add)
+
+    return new_nodes
