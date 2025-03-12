e241dd5169b91b2f76b4de7c5c1d7479c7b4df0f
Lucioschenkel <lucio.schenkel@proton.me>
2025-03-12 11:39:49 -0300

    refactor: code sharing for split_nodes_link and split_nodes_image

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import re
+from typing import List, Callable, Tuple
 
 from textnode import TextType, TextNode
 from htmlnode import LeafNode
@@ -45,14 +46,15 @@ def extract_markdown_images(text):
 def extract_markdown_links(text):
     return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
 
-def split_nodes_image(old_nodes):
+def split_nodes(nodes: List[TextNode], extract_node_func: Callable[[str], List[Tuple[str, str]]], text_type: TextType) -> List[TextNode]:
     new_nodes = []
-    for node in old_nodes:
-        images = extract_markdown_images(node.text)
+    for node in nodes:
+        # Either a link or an image
+        external_refs = extract_node_func(node.text)
         node_text = node.text
-        for image in images:
-            alt, url = image
-            [text_node_before, *after] = node_text.split(f"![{alt}]({url})")
+        for alt, url in external_refs:
+            delimiter = f"![{alt}]({url})" if text_type == TextType.IMAGE else f"[{alt}]({url})"
+            [text_node_before, *after] = node_text.split(delimiter)
             node_text = "".join(after)
             new_nodes.extend([TextNode(text_node_before, node.text_type, node.url), TextNode(alt, TextType.IMAGE, url)])
 
@@ -61,18 +63,10 @@ def split_nodes_image(old_nodes):
 
     return new_nodes
 
+
+def split_nodes_image(old_nodes):
+    return split_nodes(old_nodes, extract_markdown_images, TextType.IMAGE)
+
 def split_nodes_link(old_nodes):
-    new_nodes = []
-    for node in old_nodes:
-        links = extract_markdown_links(node.text)
-        node_text = node.text
-        for link in links:
-            alt, url = link
-            [text_node_before, *after] = node_text.split(f"[{alt}]({url})")
-            node_text = "".join(after)
-            new_nodes.extend([TextNode(text_node_before, node.text_type, node.url), TextNode(alt, TextType.LINK, url)])
+    return split_nodes(old_nodes, extract_markdown_links, TextType.LINK)
 
-        if len(node_text) > 0:
-            new_nodes.append(TextNode(node_text, node.text_type, node.url))
-
-    return new_nodes
