2718a7abdbc99b6030a4848fb3e08d54f59a071b
Lucioschenkel <lucio.schenkel@proton.me>
2025-03-21 09:24:19 -0300

    refactor: update main.py to build website into docs folder for GitHub pages support

--- src/main.py
+++ src/main.py
@@ -1,20 +1,28 @@
+import sys
+
 from fs import cleanup_directory, copy_recursive, create_directory
 from generate_page import generate_pages_recursive
 
 
 def main():
     # Cleanup public directory
-    cleanup_directory("public")
+    cleanup_directory("docs")
 
     # Re-create an empty public directory
-    create_directory("public")
+    create_directory("docs")
 
     # Recursively copy files from "static" to "public"
-    copy_recursive("static", "public")
+    copy_recursive("static", "docs")
 
     # Generate HTML from Markdown
     # generate_page("content/index.md", "template.html", "public/index.html")
-    generate_pages_recursive("content", "template.html", "public")
+    basepath = "/" if len(sys.argv) <= 1 else sys.argv[1]
+    generate_pages_recursive(
+        dir_path_content="content",
+        template_path="template.html",
+        dest_dir_path="docs",
+        basepath=basepath,
+    )
 
 
 if __name__ == "__main__":
